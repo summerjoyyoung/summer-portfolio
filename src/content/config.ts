5dd1adca2dcb7817e72753d0c4de7bfa50e80b94
Summer Young <summer.joy.young@gmail.com>
2026-04-15 19:42:11 -0700

    feat: password gate

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,7 @@ export const collections = {
 			tags: z.array(z.string()),
 			img: z.string(),
 			img_alt: z.string().optional(),
+			protected: z.boolean().optional().default(false),
 		}),
 	}),
 };
